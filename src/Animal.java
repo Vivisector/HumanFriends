146f91cae6511f030a1dd1d4070bb9b1430dec56
Dmitry Beliakov <hulk295@yandex.ru>
2023-10-04 11:32:49 +0300

    Созданы рабочие классы и методы: счетчик, меню, регистратор,домашнее животное. Все как бы работает, но теперь надо привязаться к БД. исправлен вывод все животных с вычислением их возраста и определением класса (домашние-вьючные). Не выведены пока изученные животным команды.

--- src/Animal.java
+++ src/Animal.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.util.Calendar;
 import java.time.Period;
 import java.time.ZoneId;
 import java.util.Date;
@@ -22,19 +23,38 @@ public abstract class Animal {
         return breed;
     }
 
-    public int getAge() {
-        LocalDate currentDate = LocalDate.now();
-        LocalDate birthLocalDate = birthDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        Period period = Period.between(birthLocalDate, currentDate);
-        return period.getYears(); // Возвращаем возраст в годах
+//    public int getAge() {
+//        LocalDate currentDate = LocalDate.now();
+//        LocalDate birthLocalDate = birthDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+//        Period period = Period.between(birthLocalDate, currentDate);
+//        return period.getYears(); // Возвращаем возраст в годах
+//    }
+public int calculateAgeInYears() {
+    Calendar birthCalendar = Calendar.getInstance();
+    birthCalendar.setTime(birthDate);
+
+    Calendar currentCalendar = Calendar.getInstance();
+
+    int years = currentCalendar.get(Calendar.YEAR) - birthCalendar.get(Calendar.YEAR);
+
+    // Проверяем, если текущая дата меньше даты рождения по дню и месяцу, уменьшаем возраст на 1 год
+    if (currentCalendar.get(Calendar.MONTH) < birthCalendar.get(Calendar.MONTH) ||
+            (currentCalendar.get(Calendar.MONTH) == birthCalendar.get(Calendar.MONTH) &&
+                    currentCalendar.get(Calendar.DAY_OF_MONTH) < birthCalendar.get(Calendar.DAY_OF_MONTH))) {
+        years--;
     }
 
+    return years;
+}
     public abstract void listCommands();
 
     public abstract void teachCommand(String command);
 
     @Override
     public String toString() {
-        return "Имя: " + name + ", Порода: " + breed + ", Возраст: "; // + period.getYears(); //age;
+        int age = calculateAgeInYears();
+        String animalClass = AnimalManager.determineAnimalClass(breed); // Определите класс животного здесь
+        return "Имя: " + name + ", Порода: " + breed + ", Возраст: " + age + " лет, Класс: " + animalClass;
     }
+
 }
--- src/AnimalManager.java
+++ src/AnimalManager.java
@@ -7,43 +7,52 @@ import java.util.List;
 public class AnimalManager {
     public static void addAnimal(Connection connection, String name, String species, String commands, String birthDateStr) throws SQLException {
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Date birthDate = null;
-        try {
-            birthDate = (Date) dateFormat.parse(birthDateStr);
-        } catch (ParseException e) {
-            e.printStackTrace();
-            // Обработка исключения, если преобразование не удалось
-            return; // Или другая логика обработки
-        }
-
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+//        Date birthDate = null;
+//        try {
+//            // Преобразование строки в объект java.sql.Date
+//            java.sql.Date sqlBirthDate = java.sql.Date.valueOf(birthDateStr);
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//            // Обработка исключения, если преобразование не удалось
+//            return; // Или другая логика обработки
+//        }
 
         // Определить класс животного на основе вида (по вашим правилам)
         String animalClass = determineAnimalClass(species);
 
-        // SQL-запрос для вставки данных в таблицу "Все_животные"
-        String sql = "INSERT INTO Все_животные (имя, вид, команда, дата_рождения, класс) VALUES (?, ?, ?, ?, ?)";
+        // Преобразуйте строку с датой рождения в объект java.sql.Date перед добавлением в базу данных
+        java.sql.Date sqlBirthDate = java.sql.Date.valueOf(birthDateStr);
 
-            // В зависимости от вида (species) создаем экземпляр нужного класса
-            if ("собака".equalsIgnoreCase(species)) {
-                new Dog(name, birthDate);
-            } else if ("кошка".equalsIgnoreCase(species)) {
-                new Cat(name, birthDate);
-            } else if ("хомяк".equalsIgnoreCase(species)) {
-                new Ham(name, birthDate);
-            } else if ("лошадь".equalsIgnoreCase(species)) {
-                new Horse(name, birthDate);
-            } else if ("осел".equalsIgnoreCase(species)) {
-                new Donkey(name, birthDate);
-            } else {
-                // В случае неизвестного вида животного можно вернуть null или выбрать другую стратегию
-//                return null;
-            }
+        // SQL-запрос для вставки данных в таблицу "Все_животные"
+        String sql = "INSERT INTO Все_животные (имя, вид, команда, дата_рождения) VALUES (?, ?, ?, ?)";
+
+//            // В зависимости от вида (species) создаем экземпляр нужного класса
+//            if ("собака".equalsIgnoreCase(species)) {
+//                new Dog(name, birthDate);
+//            } else if ("кошка".equalsIgnoreCase(species)) {
+//                new Cat(name, birthDate);
+//            } else if ("хомяк".equalsIgnoreCase(species)) {
+//                new Ham(name, birthDate);
+//            } else if ("лошадь".equalsIgnoreCase(species)) {
+//                new Horse(name, birthDate);
+//            } else if ("осел".equalsIgnoreCase(species)) {
+//                new Donkey(name, birthDate);
+//            }
+
+//        String sql = "INSERT INTO Все_животные (имя, вид, команда, дата_рождения) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, species);
+            preparedStatement.setString(3, commands);
+            preparedStatement.setDate(4, sqlBirthDate);
+            preparedStatement.executeUpdate();
+        }
 
     }
 
-    private static String determineAnimalClass(String species) {
-        if ("кошка".equalsIgnoreCase(species) || "собака".equalsIgnoreCase(species) || "хомяк".equalsIgnoreCase(species)) {
+    public static String determineAnimalClass(String species) {
+        if ("Кошки".equalsIgnoreCase(species) || "Собаки".equalsIgnoreCase(species) || "Хомяки".equalsIgnoreCase(species)) {
             return "домашние";
         } else {
             return "вьючные";
@@ -54,21 +63,19 @@ public class AnimalManager {
         List<Animal> animals = new ArrayList<>();
 
         // Выполните SQL-запрос для выбора всех животных из таблицы
-        String sql = "SELECT * FROM Все_животные";
+        String sql = "SELECT * FROM все_животные";
 
         try (Connection connection = DatabaseConnection.getConnection();
              Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(sql)) {
             while (resultSet.next()) {
-                // Извлекаем значения из ResultSet
-                String name = resultSet.getString("имя");
-                String species = resultSet.getString("вид");
-                String commands = resultSet.getString("команда");
-                Date birthDate = resultSet.getDate("дата_рождения");
-//                String birthDateStr = resultSet.getString("дата_рождения");
-
                 // Создайте экземпляр животного и добавьте его в список
-                Animal animal = AnimalManager.createAnimal(name, species, commands, birthDate);
+                Animal animal = AnimalManager.createAnimal(
+                        resultSet.getString("имя"),
+                        resultSet.getString("вид"),
+                        resultSet.getString("команда"),
+                        resultSet.getDate("дата_рождения")
+                );
 
                 if (animal != null) {
                     animals.add(animal);
@@ -80,30 +87,24 @@ public class AnimalManager {
     }
 
     public static Animal createAnimal(String name, String species, String commands, Date birthDateStr) {
-//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-//        Date birthDate = null;
-//        try {
-//            birthDate = (Date) dateFormat.parse(birthDateStr);
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//            // Обработка исключения, если преобразование не удалось
-//            return; // Или другая логика обработки
-//        }
+
         // В зависимости от вида (species) создаем экземпляр нужного класса
-        if ("собака".equalsIgnoreCase(species)) {
+        if ("Собаки".equalsIgnoreCase(species) || "собака".equalsIgnoreCase(species)) {
             return new Dog(name, birthDateStr);
-        } else if ("кошка".equalsIgnoreCase(species)) {
+        } else if ("Кошки".equalsIgnoreCase(species) || "кошка".equalsIgnoreCase(species)) {
             return new Cat(name, birthDateStr);
-        } else if ("хомяк".equalsIgnoreCase(species)) {
+        } else if ("Хомяки".equalsIgnoreCase(species) || "хомяк".equalsIgnoreCase(species)) {
             return new Ham(name, birthDateStr);
-        } else if ("лошадь".equalsIgnoreCase(species)) {
+        } else if ("Лошади".equalsIgnoreCase(species) || "лошадь".equalsIgnoreCase(species)) {
             return new Horse(name, birthDateStr);
-        } else if ("осел".equalsIgnoreCase(species)) {
+        } else if ("Лошади_и_Ослы".equalsIgnoreCase(species) || "лошадь".equalsIgnoreCase(species) || "осел".equalsIgnoreCase(species)) {
             return new Donkey(name, birthDateStr);
         } else {
             // В случае неизвестного вида животного можно вернуть null или выбрать другую стратегию
             return null;
         }
+
+
     }
 }
 
